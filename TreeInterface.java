49eb7ee0021bfe332a7ce6c3e41b44b630bce307
aSilverSoul <50461559+aSilverSoul@users.noreply.github.com>
2019-09-03 10:49:42 -0700

    Delete TreeInterface.java

--- TreeInterface.java
+++
@@ -1,8 +0,0 @@
-public interface TreeInterface<T> {
-
-    public T getRootData();
-    public int getHeight();
-    public int getNumberOfNodes();
-    public boolean isEmpty();
-    public void clear();
-}
